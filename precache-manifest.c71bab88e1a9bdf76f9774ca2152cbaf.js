8c3e0f11e18728d507e318e4cfbefbc03396c8be
Eve Le <nguyenthao.lee@gmail.com>
2019-12-16 00:41:54 -0800

    Updates

---
+++ precache-manifest.c71bab88e1a9bdf76f9774ca2152cbaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79668f3fc99e2df8692fafe8024876a",
+    "url": "/inspiration-board/index.html"
+  },
+  {
+    "revision": "47048259d35ae8f59916",
+    "url": "/inspiration-board/static/css/main.a27061fa.chunk.css"
+  },
+  {
+    "revision": "22a2fc43762a262d6456",
+    "url": "/inspiration-board/static/js/2.61dc5f5b.chunk.js"
+  },
+  {
+    "revision": "47048259d35ae8f59916",
+    "url": "/inspiration-board/static/js/main.416bea38.chunk.js"
+  },
+  {
+    "revision": "ff446d0d19ecaeb9679b",
+    "url": "/inspiration-board/static/js/runtime~main.ea10e4b3.js"
+  }
+]);
